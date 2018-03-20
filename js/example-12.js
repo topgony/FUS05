f58912e04da5ed8e8e84b9aac929e0a3b68282de
topgony <apdmax@naver.com>
2018-03-20 17:21:09 +0900

    모바일 메뉴 제어를 위한 버튼 완성

--- js/example-12.js
+++ js/example-12.js
@@ -3,9 +3,21 @@ $(document).ready(function(){
 
     var menubar=$('.menubar,.btn-menubar');
     var menu=$('.menu');
-    
+    var bar=$('.menubar');
+    var btn=$('.btn-menubar');
+
     menubar.on('click',function(){
         menu.toggleClass('menu-act');
-    });
+        bar.toggleClass('menubar-act');
+        if(menu.hasClass('menu-act')){
+            btn.text('메뉴 닫기');
+        }else{
+            btn.text('메뉴 열기');
+        }
     
+    });
+
+    
+
+
 });
